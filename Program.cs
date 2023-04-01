47bcf742fe612ee0265f15ac02fae9227b2e5141
JuanesZg <juanesgutierrez64@gmail.com>
2023-03-31 21:32:35 -0500

    Producto numero 2 Programacion Orientada objetos en C# primer trimestre

--- Program.cs
+++ Program.cs
@@ -1,22 +1,32 @@
-﻿//Se le solicitan al cliente que busca el prestamo los datos necesarios
+﻿//Producto 1 Proceso hacer un prestamo en una entidad financiera
+
+// //Se le solicitan al cliente que busca el prestamo los datos necesarios
+Console.WriteLine("Producto 1 - Proceso para hacer un prestamo para una entidad financiera");
 Console.WriteLine("Ingrese su nombre");
 string nombreDeudor = Console.ReadLine();
 
-Console.WriteLine("Cuentas con algun patrimonio(Propiedades, negocios, empresas, etc.)?");
-bool Patrimonio = bool.Parse(Console.ReadLine());
-
-Console.WriteLine("Tiene deudas en otros bancos?\n1.Si\n2.No");
-int Deuda = int.Parse(Console.ReadLine());
-
-Console.WriteLine("Ha tenido antecedentes penales?");
-bool antecedentesPen = bool.Parse(Console.ReadLine());
-
-
 var BancoPereira = new Prestamo(){
-    nombreCliente = nombreDeudor,
-    PatrimonioCliente = Patrimonio,
-    DeudasExternas = Deuda,
-    AntecedentesCliente = antecedentesPen
+    nombreCliente = nombreDeudor
 };
 
 BancoPereira.AutorizacionCredito();
+
+//-------------------------------------------------------------------------------------------------------------------------------------------
+
+//Producto 2 Verificar el estado de grupo de trabajo
+
+//Verificar dia de la semana - Verificar hora - Verificar estado del grupo - Lista actual de asuntos
+Console.WriteLine("Producto 2 - Proceso para verificar actividad y enviar listado de asuntos de un grupo de trabajo");
+Console.WriteLine("Ingrese el día de la semana");
+string diaSemana = Console.ReadLine();
+
+Console.WriteLine("Ingrese los asuntos que trata su grupo actualmente");
+string AsuntosdeGrupo = Console.ReadLine();
+
+var GrupoTrabajo3 = new EstadoGrupoViernes(){
+    diaDeSemana = diaSemana,
+    AsuntosGrupo = AsuntosdeGrupo
+};
+
+
+GrupoTrabajo3.EstadoGrupo();
